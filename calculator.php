3760c5034e136f935ad8336cd13ada685a91f322
Yari-Ros <2118088@talnet.nl>
2019-11-15 14:49:24 +0100

    Uitwerking van de exercise

---
+++ calculator.php
@@ -0,0 +1,12 @@
+<?php
+
+$nummer1 = readline ("");
+$wyd = readline (" ");
+$nummer2 = readline ("");
+
+
+if ($wyd == "+");
+    echo ($nummer1 + $nummer2);
+
+if ($wyd == "-");
+    echo ($nummer1 - $nummer2);
